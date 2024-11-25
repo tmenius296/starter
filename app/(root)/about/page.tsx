a14b5fa1bd4b748d2148d21b3bc45ce5cccf3494
Trenton Menius <trenton296@goutlook.com>
2024-11-25 11:20:24 -0500

    basic error handling implemented

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
 import React from "react";
 
 export default function page() {
+  throw new Error("Not Implemented");
+
   return (
     <div>
-      <h1>Users</h1>
+      <h1>About</h1>
     </div>
   );
 }
